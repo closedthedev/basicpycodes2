98cd745d024feef709be85df24b368240591b2d6
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-04-14 13:17:07 -0300

    new code

---
+++ condicionais5.py
@@ -0,0 +1,8 @@
+#descobrindo se o número é par ou ímpar
+
+num = int(input('Digite um número que vou te dizer se ele é ímpar ou par: '))
+
+if num % 2 == 0:
+    print(f'o número {num} é par!')
+
+else: print(f'o número {num} é ímpar!')
