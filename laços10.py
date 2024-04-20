34c9483f714e1ee330955e2446e0f1baacc2b334
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-04-19 22:30:36 -0300

    att

--- laços10.py
+++ laços10.py
@@ -4,9 +4,9 @@ qnt_números = int(input('Digite quantos números quer utilizar no for para real
 cont = 0
 soma = 0
 for i in range(0, qnt_números, 1):
-    cont += 1 
+    cont += 1 # a cada rodada do for, adiciona 1 a variável, no final resultando no valor de números solicitados
     i = int(input('Digite um número: '))
-    if i % 2 == 0:
+    if i % 2 == 0: #basicamente, se o valor da rodada do laço (I) for divisível por 2 (ele ser um número par) o valor de I será adicionado a variável soma.
      soma += i 
      
     
